e4a27b81fa60896c964f7fdacdff95fdc0525cdf
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-3.local>
2023-02-07 15:21:17 -0800

    error fix

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-var uniqueValidator = require('mongoose-unqiue-validator');
+var uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new Schema(
     {
