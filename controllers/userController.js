007e4d1c7e5b4e3b31b1d34c8709c5361f84cf3b
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-3.local>
2023-02-07 14:38:17 -0800

    user controller work

---
+++ controllers/userController.js
@@ -0,0 +1,65 @@
+const { User, Thought } = require('../models');
+
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({}).exec();
+        res.json(users); 
+        
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+const getSingleUser = async (req, res) => {
+    try {
+        const foundUser = await User.find({ _id: req.params.userId })
+        .populate(['friends', 'thoughts'])
+        .exec();
+
+        res.json(foundUser);
+    } catch (err) {
+        res.json(500).json(err);
+    }
+};
+
+const createUser = async (req, res) => {
+    const { username, email } = req.body;
+
+    try {
+        const newUser = await User.create({ username, email });
+
+        res.json(newUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+const updatedUser = async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.userId,
+            { $set: { ...req.body } },
+            { new: true }
+        );
+
+        updatedUser
+            ? res.json(updatedUser)
+            : res.json({ message: 'No user found' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try{
+        const deletedUser = await User.findByIdAndDelete(req.parans.userId).exec();
+
+        if (deletedUser) {
+            await Thought.deleteMany({ username: deleteduser.username });
+
+            res.json(deletedUser);
+        } else res.json({ message: 'No user deleted' });
+    } catch (err) {
+        res.json(500).json(err);
+    }
+}
