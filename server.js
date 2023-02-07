203f9f443f69d5041ccfa8dce08a721c819c828c
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-3.local>
2023-02-07 11:47:26 -0800

    server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(expres.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    console.log(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+});
